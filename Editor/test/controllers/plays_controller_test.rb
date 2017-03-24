13b4fa5675c98162b25f14aad2329bdcf0dcd61d
cs laptop <cslaptop@WL-219-45.WPA.Claremont.Edu>
2017-03-24 11:05:09 -0700

    Removed Editor folder

--- Editor/test/controllers/plays_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PlaysControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get plays_index_url
-    assert_response :success
-  end
-
-end
