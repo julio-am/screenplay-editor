6e9531733041eac4a262fd67d41fd0a358301c9c
cs laptop <cslaptop@WL-198-198.WPA.Claremont.Edu>
2017-04-02 14:02:13 -0700

    Made a working parser. Writes to written.txt

--- app/views/plays/parser.py
+++ app/views/plays/parser.py
@@ -1,28 +1,49 @@
 import xml.etree.ElementTree as ET
+#from sys import argv
 
+def stageDir(content, target):
+    xmlstr = ET.tostring(content, encoding='utf8', method='text')
+    target.write("\nStage Directions %s" % filter(lambda x: x != "\n", xmlstr).replace("  ", ""))
+
+def speaker(content, target):
+    xmlstr = ET.tostring(content, encoding='utf8', method='text')
+    target.write("\nSpeaker ")
+    target.write(filter(lambda x: x != "\n", xmlstr).replace("  ", ""))
+
+def lines(content, target):
+    line = ""
+    for words in content.findall("./*"):
+        if ((words.tag == "{http://www.tei-c.org/ns/1.0}milestone") and (words.get('unit') != "page")):
+            target.write(filter(lambda x: x != "\n", line).replace("  ", ""))
+            target.write("\n%s " % words.get('n'))
+            line = ""
+        elif (words.tag != "{http://www.tei-c.org/ns/1.0}fw"):
+            line += ET.tostring(words, encoding='utf8', method='text')
+    target.write(filter(lambda x: x != "\n", line).replace("  ", ""))
+
+
+def printOneScene(scene, target):
+    for content in scene.iter():
+        if (content.tag == "{http://www.tei-c.org/ns/1.0}stage"):
+            stageDir(content, target)
+        elif (content.tag == "{http://www.tei-c.org/ns/1.0}speaker"):
+            speaker(content, target)
+        elif(content.tag == "{http://www.tei-c.org/ns/1.0}ab"):
+            lines(content, target)  
+
+target = open("written.txt", "w")
+target.truncate()
 tree = ET.parse("data.xml").getroot()
 ET.register_namespace("{http://www.tei-c.org/ns/1.0}", "http://www.tei-c.org/ns/1.0")
 acts = tree.findall(".//{http://www.tei-c.org/ns/1.0}div1")
 for act in acts:
-	print "Act", act.get('n')
-	for scene in act.findall(".//{http://www.tei-c.org/ns/1.0}div2"):
-		print "Scene", scene.get('n')
-		for content in scene.iter():
-			if (content.tag == "{http://www.tei-c.org/ns/1.0}stage"):
-				string = ""
-				xmlstr = ET.tostring(content, encoding='utf8', method='text')
-				print "Stage Directions", filter(lambda x: x != "\n", xmlstr)
-			elif (content.tag == "{http://www.tei-c.org/ns/1.0}speaker"):
+    target.write("Act ")
+    target.write(act.get('n'))
+    target.write("\n")
+    for scene in act.findall(".//{http://www.tei-c.org/ns/1.0}div2"):
+        target.write("Scene ")
+        target.write(act.get('n'))
+        target.write("\n")
+        printOneScene(scene, target)
+target.close()
 
-				xmlstr = ET.tostring(content, encoding='utf8', method='text')
-				print "Speaker", filter(lambda x: x != "\n", xmlstr)
-			elif(content.tag == "{http://www.tei-c.org/ns/1.0}ab"):
-				
-				xmlstr = ET.tostring(content, encoding='utf8', method='text')
-				print "Line", filter(lambda x: x != "\n", xmlstr)
-
-
-
-
-			
-			
