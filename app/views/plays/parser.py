f72cb32a4c1754ff11fc4456b2fa994959ce60f7
cs laptop <cslaptop@WL-214-79.WPA.Claremont.Edu>
2017-04-04 20:45:07 -0700

    Got parser to work and display a play

--- app/views/plays/parser.py
+++ app/views/plays/parser.py
@@ -2,11 +2,11 @@ import xml.etree.ElementTree as ET
 
 def stageDir(content, target):
     xmlstr = ET.tostring(content, encoding='utf8', method='text')
-    target.write("\nStage Directions %s" % filter(lambda x: x != "\n", xmlstr).replace("  ", ""))
+    target.write("\n<br>\nStage Directions %s" % filter(lambda x: x != "\n", xmlstr).replace("  ", ""))
 
 def speaker(content, target):
     xmlstr = ET.tostring(content, encoding='utf8', method='text')
-    target.write("\nSpeaker ")
+    target.write("\n<br>\nSpeaker ")
     target.write(filter(lambda x: x != "\n", xmlstr).replace("  ", ""))
 
 def getLines(content, target):
@@ -14,7 +14,7 @@ def getLines(content, target):
     for words in content.findall("./*"):
         if ((words.tag == "{http://www.tei-c.org/ns/1.0}milestone") and (words.get('unit') != "page")):
             target.write(filter(lambda x: x != "\n", line).replace("  ", ""))
-            target.write("\n%s " % words.get('n'))
+            target.write("\n<br>\n%s " % words.get('n'))
             line = ""
         elif (words.tag != "{http://www.tei-c.org/ns/1.0}fw"):
             line += ET.tostring(words, encoding='utf8', method='text')
@@ -30,19 +30,20 @@ def printOneScene(scene, target):
         elif(content.tag == "{http://www.tei-c.org/ns/1.0}ab"):
             getLines(content, target)  
 
-target = open("written.txt", "w")
+header = open("header.html", "r")
+lines = header.readlines()
+target = open("index.html.erb", "w")
 target.truncate()
+for line in lines:
+    target.write(line)
 tree = ET.parse("data.xml").getroot()
 ET.register_namespace("{http://www.tei-c.org/ns/1.0}", "http://www.tei-c.org/ns/1.0")
 acts = tree.findall(".//{http://www.tei-c.org/ns/1.0}div1")
 for act in acts:
-    target.write("Act ")
-    target.write(act.get('n'))
-    target.write("\n")
+    target.write("\n<h1>\nAct %s\n</h1>" % act.get('n'))
     for scene in act.findall(".//{http://www.tei-c.org/ns/1.0}div2"):
-        target.write("Scene ")
-        target.write(act.get('n'))
-        target.write("\n")
+        target.write("\n<h2>\nScene %s\n</h2>" % act.get('n'))
         printOneScene(scene, target)
+target.write("</body>\n</html>")
 target.close()
 
