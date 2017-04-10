3bb4d52f62886b9ab8afd187b722afe23f407552
Samantha Andow <samantha.andow@gmail.com>
2017-04-09 20:51:31 -0700

    Commented parser through

--- app/views/plays/parser.py
+++ app/views/plays/parser.py
@@ -1,25 +1,42 @@
 import xml.etree.ElementTree as ET
 
+"""
+elemToString
+This takes in content, a node, and returns the inner text
+"""
 def elemToString(content):
     return ET.tostring(content, encoding='utf8', method='text')
 
+"""
+cleanElemToString
+This takes in content, a node, and returns the inner text with only one space between
+words and no line breaks
+"""
 def cleanElemToString(content):
     string = elemToString(content)
     return filter(lambda x: x != "\n", string).replace("  ", "")
 
+"""
+printSingleLine
+This writes a string to file after removing extra spaces and all line breaks
+This takes in line, a string, and targetFile, a file object with write privileges.
+"""
 def printSingleLine(line, targetFile):
     targetFile.write(filter(lambda x: x != "\n", line).replace("  ", ""))
 
 """
 outerLvlStageDir 
-This gets the stage directions in the middle of a line and writes them to our file.
-This takes in content and a targetFile.
+This gets the stage directions that are not in the middle of a line and writes them to our file.
+This takes in content, a stage directions XML node, and a targetFile, the file object with write privileges.
 """
 def outerLvlStageDir(content, targetFile):
+    # Because there is no parent function for elementTree, we had to make our own
     parents = content.findall("..")
     for parent in parents:
+        # This checks that the stage direction is not in a line
         if(parent.tag != "ab"):
-            xmlstr = cleanElemToString(content)
+            xmlstr = cleanElemToString(content)            
+            # If the stage direction starts with a comma, we write on the same line as the character name
             if xmlstr[0] != ',':
                 targetFile.write("\n<br>\n")
                 targetFile.write("<i>%s</i>" % xmlstr)
@@ -28,18 +45,19 @@ def outerLvlStageDir(content, targetFile):
 """
 stageDirInLine 
 This gets the stage directions in the middle of a line and writes them to our file.
-This takes in content and a targetFile.
+This takes in content, a stage directions XML node,  and a targetFile, the file object with write privileges.
 """
 def stageDirInLine(content, targetFile):
     xmlstr = cleanElemToString(content)
+    # If the stage direction starts with a comma, we write on the same line as the character name
     if xmlstr[0] != ',':
         targetFile.write("\n<br>\n")
         targetFile.write("<i>%s</i>" % xmlstr)
 
 """
 speaker
-This gets the speaker of a line and write it to our file.
-This takes in content and a targetFile.
+This writes the speaker's name to file and returns it to use as the key for the dictionary.
+This takes in content, a speaker node, and a targetFile, a file object with write privileges.
 """
 def speaker(content, targetFile):
         xmlstr = cleanElemToString(content)
@@ -48,45 +66,53 @@ def speaker(content, targetFile):
 
 """
 getLines
-This will get lines and stage directions and call printSingleLine on them.
-It takes in content and a targetfile.
+This will write all the lines that one character speaks and the in-line stage directions to a file.
+It takes in content, a node with tag 'ab', and a targetFile, a file object with write privilege.
 """
 def getLines(content, targetFile):
     line = ""
     numLines = 0
     for words in content.findall("./*"):
+        # If the child is a milestone, it prints out the previous line, the next line number, and resets
         if ((words.tag == "{http://www.tei-c.org/ns/1.0}milestone") and (words.get('unit') == "ftln")):
             numLines += 1
             printSingleLine(line, targetFile)
             targetFile.write('\n<br>\n<span class="lineNum">%s</span>' % words.get('n'))
             line = ""
-        elif((words.tag == "{http://www.tei-c.org/ns/1.0}q")):
+        # If the child node is a q or seg, those are wrappers, so we need to go one level deeper
+        elif((words.tag == "{http://www.tei-c.org/ns/1.0}q") or (words.tag == "{http://www.tei-c.org/ns/1.0}seg")):
             getLines(words, targetFile)
+        # If the child is a stage, we should print the line and then print the stage direction
         elif (words.tag == "{http://www.tei-c.org/ns/1.0}stage"):
             printSingleLine(line, targetFile)
             line = ""
             stageDirInLine(words, targetFile)
-        elif(words.tag == "{http://www.tei-c.org/ns/1.0}seg"):
-            getLines(words, targetFile)
+        # Any other tag that is not fw is a word, space, or punctuation that should be added to the line
         elif (words.tag != "{http://www.tei-c.org/ns/1.0}fw"):
             line += ET.tostring(words, encoding='utf8', method='text')
+    # Because we never hit a final milestone after reading in the last line, we need to print it out
     printSingleLine(line, targetFile)
     return numLines
 
 """
 printOneScene
-This will write a single scene as we want it formatted.
-It takes in a scene, a targetFile, and a dictionary.
+This will write a single scene as we want it formatted and update the character line dictionary.
+It takes in a scene (div2) node, a file to write to, and a dicitionary that holds the lines characters.
 """
 def writeOneScene(scene, targetFile, dictionary):
     curSpeaker = ""
     lines = 0
+    # This goes through every node in the scene, hence the need for outerLvlStageDir and stageDirInLine
     for content in scene.iter():
+        # If we get a stage direction at this level, it should be an outer level one
         if (content.tag == "{http://www.tei-c.org/ns/1.0}stage"):
             outerLvlStageDir(content, targetFile)
+        # If we get a speaker, we need to update the current speaker
         elif (content.tag == "{http://www.tei-c.org/ns/1.0}speaker"):
             curSpeaker = speaker(content, targetFile)
+        # If we get an 'ab' tag, this is the start of a line for curSpeaker
         elif(content.tag == "{http://www.tei-c.org/ns/1.0}ab"):
+            # Writes the line to the targetFile and updates the character dictionary
             lines = getLines(content, targetFile)
             if curSpeaker not in dictionary:
                 dictionary[curSpeaker] = lines
@@ -120,25 +146,37 @@ def visitAct(xmlTree, targetFile):
             targetFile.write(thirdLvl + '<li><a href="#'+idNumber)
             targetFile.write('">Scene %s</a></li>\n' % scene.get('n'))
         targetFile.write(secondLvl+'</ul>\n'+baseIndent+'</div>\n')
+        # Every 3 acts, we will create a new row
         if int(act.get('n')) == 3:
             targetFile.write(secondLvl+"</div>")
-
-
+            
+            
 dictionary = {}
 header = open("header.html", "r")
 lines = header.readlines()
 target = open("index.html.erb", "w")
 tree = ET.parse("data.xml").getroot()
+
+# Write the header to index file first, using the visitor parser at the appropriate place
 for line in lines:
     target.write(line)
     if '<div class="row">' in line:
         visitAct(tree, target)
+
+# FIXME: Make namespaces work
 ET.register_namespace("{http://www.tei-c.org/ns/1.0}", "http://www.tei-c.org/ns/1.0")
+
+# Start by finding all the acts, noted with div1's
 acts = tree.findall(".//{http://www.tei-c.org/ns/1.0}div1")
+
 for act in acts:
     target.write("\n<h1>\nAct %s\n</h1>" % act.get('n'))
+    # Find all the scenes in the act. Each has the tag div2
     scenes = act.findall(".//{http://www.tei-c.org/ns/1.0}div2")
+    
     for scene in scenes:
+        # idNumber is the id attribute so the navigation works.
+        # It reflects the ActNumber.SceneNumber numbering of Shakespeare plays
         idNumber = act.get('n') + "." + scene.get('n')
         target.write("\n<h2 id ="+idNumber+">\nScene %s\n</h2>" % scene.get('n'))
         writeOneScene(scene, target, dictionary)
@@ -148,6 +186,9 @@ target.close()
 chars = open("characters.html", "w")
 chars.write("<DOCTYPE! HTML>\n<html>")
 chars.write('<table style="width:100%">\n')
+
+# In a table we output the name of the character from the dictionary
+# and the number of lines they spoke
 for key in dictionary:
     chars.write('<tr><td>%s</td>\n' % key)
     chars.write('<td>%d</td></tr>\n' % dictionary[key])
